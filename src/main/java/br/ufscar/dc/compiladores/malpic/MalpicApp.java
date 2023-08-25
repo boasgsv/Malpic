e66efc55b279a645ff6bb394f14e94c81025f9b8
boasgsv <boasgsv@protonmail.com>
2023-08-25 08:23:48 -0300

    update: various java source features

--- src/main/java/br/ufscar/dc/compiladores/malpic/MalpicApp.java
+++ src/main/java/br/ufscar/dc/compiladores/malpic/MalpicApp.java
@@ -4,8 +4,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+import br.ufscar.dc.compiladores.malpic.listeners.MyCustomErrorListener;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
 
 
@@ -15,6 +17,31 @@ import org.antlr.v4.runtime.Token;
  */
 public class MalpicApp
 {
+    public static void main(String [] args)
+    {
+        try {
+            CharStream cs = CharStreams.fromFileName(args[0]);
+            MalpicLexer lexer = new MalpicLexer(cs);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            MalpicParser parser = new MalpicParser(tokens);
+
+            String filename = args[1];
+            FileWriter writer = new FileWriter(filename);
+
+            //Configuração do erro customizado
+            MyCustomErrorListener mcel = new MyCustomErrorListener(writer);
+            parser.removeErrorListeners();
+            parser.addErrorListener(mcel);
+
+            parser.program();
+
+            writer.close();
+        } catch(IOException ex) {
+            System.out.println("Exception: " + ex.getMessage());
+        }
+    }
+
+    /*
     public static void main( String[] args )
     {
         try{
@@ -48,4 +75,7 @@ public class MalpicApp
             System.out.println(ex.getMessage());
         }
     }
+    */
+
+
 }
